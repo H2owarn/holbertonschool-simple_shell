c5c0eed7c0b7c8fa2c49fa7eb9d42f7d5e4725d9
wa <chutaphat@hotmail.com>
2025-02-20 10:57:51 +0000

    print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * This function prints a string to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+}
