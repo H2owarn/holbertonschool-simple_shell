ba53babb77288aec0214297f372898561053487a
wa <chutaphat@hotmail.com>
2025-02-23 05:08:38 +0000

    print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - make the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch = ch + 1)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
