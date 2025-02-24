528ba0471750b6f837ae7c54cc7a09fe2c62611f
wa <chutaphat@hotmail.com>
2025-02-24 04:42:37 +0000

    multiplies

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *mul - multiplies two integers.
+ *@a: The character to check
+ *@b: The charactor
+ *
+ *Return: a*b
+ */
+int mul(int a, int b)
+{
+return(a*b);
+}
