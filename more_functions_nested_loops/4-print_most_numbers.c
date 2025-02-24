5644127c77aa5829b0ffc5b7c3cb3dda8bf592f5
wa <chutaphat@hotmail.com>
2025-02-24 11:07:11 +0000

    skipt some number

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  *print_most_numbers - prints the numbers, from 0 to 9.
- *@n : number 
+ *
  *
  *
  *
@@ -11,7 +11,7 @@
 void print_most_numbers(void)
 {
 int n;
-for (n = 0; n <= 9; n = n + 1 )
+for (n = 0; n <= 9; n = n + 1)
 if (n != 2 && n != 4)
 {
 _putchar(n + '0');
