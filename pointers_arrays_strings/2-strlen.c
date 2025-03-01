40cc5428973e7f0ce1786f2f55ace2673dfc163f
wa <chutaphat@hotmail.com>
2025-03-01 03:46:28 +0000

    while len

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -3,9 +3,8 @@
 /**
  * _strlen - returns the length of a string.
  * @s: Pointer to an integer that will be updated.
- * 
  *
- * Description: This function returns the length of a string.
+ * Return: len
  */
 int _strlen(char *s)
 {
@@ -14,5 +13,5 @@ while (s[len] != '\0')
 {
 len = len + 1;
 }
-return len;
+return (len);
 }
