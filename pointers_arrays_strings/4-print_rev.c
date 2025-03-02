25f77be197f152a5bc2694b2cd9d8b116f4b52cc
wa <chutaphat@hotmail.com>
2025-03-02 03:01:19 +0000

    keep it simple

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,8 +10,12 @@
  */
 void print_rev(char *s)
 {
-int len = strlen(s);
 int i;
+int len = 0;
+while (s[len] != '\0')
+{
+len = len + 1;
+}
 for (i = len - 1; i >= 0; i = i - 1)
 {
 _putchar(s[i]);
