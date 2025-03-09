bbeb0e2b3cfd0b25bda1c628f272d32bbb75217a
wa <chutaphat@hotmail.com>
2025-03-09 08:45:18 +0000

    argc-1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - print number of arguments
+ * @argc: number
+ * @argv: void
+ *
+ *
+ * Return: argv - 1
+ */
 int main(int argc, char *argv[])
 {
 	(void)argv;
