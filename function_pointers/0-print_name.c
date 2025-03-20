4178f2d9894937cdf4dafb8694a837d5e5641f43
wa <chutaphat@hotmail.com>
2025-03-20 08:20:40 +0000

    pointer

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include <stddef.h>
 #include "function_pointers.h"
 /**
- * print_name - prints a name
+ * print_name - prints a name using a specified function
  * @name: name of the person
- * @f: function pointer
+ * @f: function pointer to specify how to print the name
  *
- * Return: nothing
+ * Description: This function checks for null pointers and uses
+ * the provided function to print the name in a specific format.
  */
 void print_name(char *name, void (*f)(char *))
 {
