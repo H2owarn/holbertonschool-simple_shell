2c09460b4144b4e417f8f61fd2aea6b0df4e21d7
wa <chutaphat@hotmail.com>
2025-03-23 23:46:45 +0000

    simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,25 +3,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 /**
- * print_number - prints an integer using _putchar
- * @n: The integer to print
- *
- * Return: Nothing
- */
-void print_number(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
-}
-/**
  * main- Performs simple operations
  * @argc: Number
  * @argv: Array
@@ -31,22 +12,25 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 	int (*operation)(int, int);
+	char buffer[12];
+	int len;
 
 	if (argc != 4)
 	{
 		write(1, "Error\n", 6);
 		exit(98);
 	}
+
+	operation = get_op_func(argv[2]);
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	operation = get_op_func(argv[2]);
-	if (operation == NULL)
+	if (!operation)
 	{
 		write(1, "Error\n", 6);
 		exit(99);
 	}
 	result = operation(num1, num2);
-	print_number(result);
-	_putchar('\n');
+	len = sprintf(buffer, "%d\n", result);
+	write(1, buffer, len);
 	return (0);
 }
