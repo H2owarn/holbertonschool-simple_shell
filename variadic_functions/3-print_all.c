20b76034a8eb17b2d1130251f37640064f6f8f37
wa <chutaphat@hotmail.com>
2025-03-24 02:40:32 +0000

    prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_string - Prints a string using _putchar.
+ * @str: The string to print. Prints "(nil)" if the string is NULL.
+ */
+void print_string(const char *str)
+{
+if (!str)
+{
+print_string("(nil)");
+return;
+}
+while (*str)
+{
+_putchar(*str++);
+}
+}
+/**
+ * print_number - Prints an integer using _putchar.
+ * @n: The integer to print.
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + '0');
+}
+/**
+ * print_all - Prints anything based on the format.
+ * @format: A list of types
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str, *sep = "";
+
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == 'c' || format[i] == 'i' ||
+format[i] == 'f' || format[i] == 's'){
+		print_string(sep);
+		switch (format[i])
+		{
+			case 'c':
+				_putchar(va_arg(args, int));
+				break;
+			case 'i':
+				print_number(va_arg(args, int));
+				break;
+			case 'f': {
+					  double f = va_arg(args, double);
+					  int whole = (int)f;
+					  int decimal = (int)((f - whole) * 1000000);
+
+					  print_number(whole);
+					  _putchar('.');
+					  print_number(decimal);
+break;
+}
+			case 's':
+				 str = va_arg(args, char *);
+				 print_string(str ? str : "(nil)");
+break;
+		}
+		sep = ", ";
+		}
+		i++;
+	}
+	va_end(args);
+	_putchar('\n');
+}
