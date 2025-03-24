e88026726580ce903644f53f77cc298e160edc7e
wa <chutaphat@hotmail.com>
2025-03-24 01:08:33 +0000

    _putchar

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,17 +3,6 @@
 #include "variadic_functions.h"
 #include <unistd.h>
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to print.
- *
- * Return: On success, 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
  * print_string - Prints a string using _putchar.
  * @str: The string to print.
  */
