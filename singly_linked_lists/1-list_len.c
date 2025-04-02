6e354572a69d8630b6bdbe827a7d4e731d6929a0
wa <chutaphat@hotmail.com>
2025-04-02 13:10:20 +0000

    list_len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,58 +1,19 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
 #include <stddef.h>
+#include "lists.h"
 /**
- * print_number - Prints an integer using _putchar.
- * @n: The integer to print.
- */
-void print_number(int n)
-{
-	unsigned int num = n;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-	}
-	if (num / 10)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
-}
-/**
- * print_string - Prints a string using _putchar.
- * @str: The string to print. If NULL, prints "(nil)".
- */
-void print_string(const char *str)
-{
-	if (!str)
-		str = "(nil)";
-	while (*str)
-	{
-		_putchar(*str);
-		str++;
-	}
-}
-/**
- * print_list - Prints a singly linked list using _putchar.
- * @h: The head of the linked list.
+ * list_len - Returns the number of elements in a linked list.
+ * @h: Pointer to the head of the linked list.
  *
- * Return: The number of nodes.
+ * Return: Number of elements in the linked list.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h)
 	{
-		print_string("[");
-		print_number(h->len);
-		pritn_string("]");
-		print_string(h->str);
-		_putchar('\n');
-		h = h->next;
 		count++;
+		h = h->next;
 	}
-	return (count);
+	return count;
 }
