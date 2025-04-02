2d255d08f36c85f603e57d96c0fc294fa5a9d970
wa <chutaphat@hotmail.com>
2025-04-02 12:55:43 +0000

    Des

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,17 +1,25 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stddef.h>
-
-/* Structure for a singly linked list */
+/**
+ * struct list_s - Singly linked list node
+ * @str: String data stored in the node (malloc'ed string)
+ * @len: Length of the string
+ * @next: Pointer to the next node in the list
+ *
+ * Description: This structure represents a node in a singly linked list,
+ * where each node contains a string, its length
+ * and a pointer to the next node.
+ */
 typedef struct list_s
 {
-    char *str;               /* String - malloc'ed */
-    unsigned int len;        /* Length of the string */
-    struct list_s *next;     /* Points to the next node */
+	char *str;               /* String - malloc'ed */
+	unsigned int len;        /* Length of the string */
+	struct list_s *next;     /* Points to the next node */
 } list_t;
 
 /* Function prototypes */
-size_t print_list(const list_t *h); /* Prints the linked list and returns number of nodes */
+size_t print_list(const list_t *h);
 void print_number(int n);           /* Prints an integer using _putchar */
 void print_string(const char *str); /* Prints a string using _putchar */
 int _putchar(char c);               /* Prints a single character */
