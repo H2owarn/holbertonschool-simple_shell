c17328f073db489d78af74b6ac03357e382d2df1
wa <chutaphat@hotmail.com>
2025-04-03 03:44:38 +0000

    doubly

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_number - Prints an integer using _putchar.
- * @n: The integer to print.
- */
-void print_number(int n)
-{
-	unsigned int num = n;
-
-	if (n < 0) /* Handle negative numbers */
-	{
-		_putchar('-');
-		num = -n;
-	}
-	if (num / 10) /* Recursively print digits */
-		print_number(num / 10);
-	_putchar((num % 10) + '0'); /* Print the last digit */
-}
-/**
  * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: Pointer to the head of the doubly linked list.
  *
@@ -29,8 +12,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h)
 	{
-	print_number(h->n)
-	_putchar('\n');
+	printf("%d\n", h->n);
 	count++;
 	h = h->next;
 	}
