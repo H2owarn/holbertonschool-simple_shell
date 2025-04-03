38a58412bf224eeaa21ccd15cc86b3497055cb2a
wa <chutaphat@hotmail.com>
2025-04-03 15:07:04 +0000

    add

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,8 +10,8 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
-	new_node = malloc(sizeof(dlistint_t));
 
+	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
