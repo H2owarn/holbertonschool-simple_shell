830451a9ada31689b63c21ee8604d57d4237dbaf
wa <chutaphat@hotmail.com>
2025-04-04 17:13:37 +0000

    insert

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,12 +31,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 			new_node = malloc(sizeof(dlistint_t));
 			if (!new_node)
+			{
 				return (NULL);
-				new_node->n = n;
-				new_node->next = temp->next;
-				new_node->prev = temp;
-				if (temp->next)
-					temp->next->prev = new_node;
-					temp->next = new_node;
-					return (new_node);
+			}
+			new_node->n = n;
+			new_node->next = temp->next;
+			new_node->prev = temp;
+			if (temp->next)
+			{
+				temp->next->prev = new_node;
+			}
+			temp->next = new_node;
+			return (new_node);
 }
