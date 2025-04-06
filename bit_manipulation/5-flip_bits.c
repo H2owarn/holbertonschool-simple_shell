5c2067a35bb0f3aea65aed1cd5a58c5075d1692e
wa <chutaphat@hotmail.com>
2025-04-06 08:04:37 +0000

    fix coed

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xor_result = n ^ m;
 	unsigned int count = 0;
 
-	while (xor_resullt)
+	while (xor_result)
 	{
 		count += xor_result & 1;
 		xor_result >>= 1;
