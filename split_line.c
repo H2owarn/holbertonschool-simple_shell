d7a107b9f10fc03cccb63e35551ac72bcb0166c3
wa <chutaphat@hotmail.com>
2025-04-25 10:12:47 +0000

    multiple commands

--- split_line.c
+++ split_line.c
@@ -2,11 +2,22 @@
 
 char **split_line(char *line)
 {
-    char **tokens = malloc(2 * sizeof(char *));
+    char **tokens = malloc(64 * sizeof(char *));
+    char *token;
+    int position = 0;
+
     if (!tokens)
-        return NULL;
-    
-    tokens[0] = strtok(line, "\n");
-    tokens[1] = NULL;
+    {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, " \t\n");
+    while (token != NULL)
+    {
+        tokens[position++] = token;
+        token = strtok(NULL, " \t\n");
+    }
+    tokens[position] = NULL;
     return tokens;
 }
