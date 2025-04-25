7a102db307ef8942c6c4afc6db0403e17dca4da3
wa <chutaphat@hotmail.com>
2025-04-25 14:02:04 +0000

    compile

--- split_line.c
+++ split_line.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include "shell.h"
+
 char **split_line(char *line)
 {
     int bufsize = 64;
