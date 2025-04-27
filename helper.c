127a9c77f90d593d413d1ceba33ce75e7892f9b6
wa <chuta@hotmail>
2025-04-27 11:52:15 +0000

    helper

---
+++ helper.c
@@ -0,0 +1,37 @@
+/* _strlen */
+int _strlen(const char *s)
+{
+    int len = 0;
+    while (s && s[len])
+        len++;
+    return len;
+}
+
+/* _strchr */
+char *_strchr(const char *s, char c)
+{
+    while (s && *s)
+    {
+        if (*s == c)
+            return ((char *)s);
+        s++;
+    }
+    return NULL;
+}
+
+/* _strdup */
+char *_strdup(const char *s)
+{
+    char *copy;
+    int len = _strlen(s);
+    int i;
+
+    copy = malloc(len + 1);
+    if (!copy)
+        return NULL;
+    for (i = 0; i < len; i++)
+        copy[i] = s[i];
+    copy[i] = '\0';
+    return copy;
+}
+
