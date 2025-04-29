1d32c0e78b4e5e71bb0f2ea246a5b5d08caf6fcc
wa <chutaphat@hotmail.com>
2025-04-29 00:52:46 +0000

    header

--- utils.c
+++ utils.c
@@ -1,8 +1,12 @@
 #include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
 /**
  * get_path_env - Retrieve the value of the
